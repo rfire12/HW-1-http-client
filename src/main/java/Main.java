07137b3ebc15c5e9f008af365e1239d98cdddff3
Raul <rapraulra@gmail.com>
2019-09-27 02:13:37 -0400

    Identifying each form inputs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,8 @@ public class Main {
         System.out.println(String.format("La pagina contiene %d imagenes dentro de los parrafos", getImagesInParagraphs(htmlDocument)));
         System.out.println(String.format("La pagina contiene %d formularios con el metodo POST", getForms(htmlDocument, "post")));
         System.out.println(String.format("La pagina contiene %d formularios con el metodo GET", getForms(htmlDocument, "get")));
+        System.out.println();
+        printFormsInputsType(htmlDocument);
     }
 
     private static int getNumberOfLines(String document){
@@ -31,7 +33,7 @@ public class Main {
     }
 
     private static int getForms(Document document, String method){
-        Elements forms = document.select("form"); // Getting the forms from the document
+        Elements forms = document.select("form"); // Getting forms from the document
         int formsFound = 0;
         for(Element form: forms) {
             if(form.attr("method").equalsIgnoreCase(method))
@@ -40,8 +42,20 @@ public class Main {
         return formsFound;
     }
 
-
-
+    private static void printFormsInputsType(Document document){
+        Elements forms = document.select("form");
+        int formIndex = 1;
+        int inputIndex = 1;
+        for(Element form : forms){
+            System.out.println(String.format("Formulario #%d: Method: %s", formIndex, form.attr("method")));
+            for(Element input: form.select("input")){
+                System.out.println(String.format("Input type #%d: %s \n",inputIndex, input.attr("type")));
+                inputIndex++;
+            }
+            formIndex++;
+            inputIndex = 1; // Restart the input index each time we change the form
+        }
+    }
 
 
 
