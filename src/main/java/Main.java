9d84877a0f2861a8e9c1495957504c7d2a20a9e6
Raul <rapraulra@gmail.com>
2019-09-27 03:15:51 -0400

    All done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,20 +4,25 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String [] args) throws IOException {
-        String url = "http://itachi.avathartech.com:4567/opcion2.html";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("URL: ");
+        String url = scanner.nextLine();
         Document htmlDocument = Jsoup.connect(url).timeout(6000).get();
 
-        System.out.println(String.format("La pagina contiene %d lineas", getNumberOfLines(htmlDocument.toString())));
+        System.out.println(String.format("La pagina contiene %d lineas", getNumberOfLines(Jsoup.connect(url).execute().body())));
         System.out.println(String.format("La pagina contiene %d parrafos", getNumberOfParagraphs(htmlDocument)));
         System.out.println(String.format("La pagina contiene %d imagenes dentro de los parrafos", getImagesInParagraphs(htmlDocument)));
         System.out.println(String.format("La pagina contiene %d formularios con el metodo POST", getForms(htmlDocument, "post")));
         System.out.println(String.format("La pagina contiene %d formularios con el metodo GET", getForms(htmlDocument, "get")));
         System.out.println();
         printFormsInputsType(htmlDocument);
+        makeRequest(htmlDocument, "http://itachi.avathartech.com:4567/opcion2.html");
     }
 
     private static int getNumberOfLines(String document){
@@ -44,8 +49,7 @@ public class Main {
 
     private static void printFormsInputsType(Document document){
         Elements forms = document.select("form");
-        int formIndex = 1;
-        int inputIndex = 1;
+        int formIndex = 1, inputIndex = 1;
         for(Element form : forms){
             System.out.println(String.format("Formulario #%d: Method: %s", formIndex, form.attr("method")));
             for(Element input: form.select("input")){
@@ -57,6 +61,36 @@ public class Main {
         }
     }
 
+    private static void makeRequest(Document document, String url){
+        Elements forms = document.select("form");
 
+        for(Element form : forms) {
+            if(form.attr("method").equalsIgnoreCase("post")){
+                url = parseURL(form, url);
+                System.out.println(url);
+                try{
+                    Document response = Jsoup.connect(url).data("asignatura", "practica1").header("matricula", "20160792").post();
+                    System.out.println(response);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
 
+            }
+
+        }
+    }
+
+    private static String parseURL(Element form, String url) {
+        try{
+            URL newUrl = new URL(url);
+            if(form.attr("action").contains("http")) //If the form action contains a full URL
+                url = form.attr("action"); // Use the form URL
+            else
+                url = newUrl.getProtocol() + "://" + newUrl.getHost() + ":" + newUrl.getPort() + form.attr("action"); // Append the form action to the given URL by the user
+            return url;
+        } catch (Exception e){
+            System.out.println("Error parsing url");
+        }
+        return url;
+    }
 }
